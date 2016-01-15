195ee7785c66177103a9b9ea7ddd9263cf055c52
CuteITGuy <grisetroseciel@gmail.com>
2016-01-15 20:01:23 +0700

    First commit

---
+++ ScriptHelper/ScriptHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace QA.Tools
+{
+    public class ScriptHelper
+    {
+        #region Methods
+        public static string GetDatabaseName(string script)
+        {
+            var match = Regex.Match(script, @"(?i)use\s+\[?(\w+)\]?");
+            return GetLastCapture(match);
+        }
+
+        public static string GetStoredProcedureName(string script)
+        {
+            var match = Regex.Match(script, @"(?i)\bprocedure\b\s+(?:\[?dbo\]?\.)?\[?(\w+)\]?");
+            return GetLastCapture(match);
+        }
+        #endregion
+
+
+        #region Implementation
+        private static string GetLastCapture(string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            var match = Regex.Match(input, pattern, options);
+            return GetLastCapture(match);
+        }
+
+        private static string GetLastCapture(Match match)
+        {
+            return match.Groups[match.Groups.Count - 1].Value;
+        }
+        #endregion
+    }
+}
